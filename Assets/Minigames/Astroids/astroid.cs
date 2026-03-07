012c15bfb3fa791f6c7e34e11751af2c95f1c0a6
SoveriegnHippo <zacharymueller3208@gmail.com>
2026-03-07 17:35:46 -0600

    Made the astroids Game

---
+++ Assets/Minigames/Astroids/astroid.cs
@@ -0,0 +1,57 @@
+using TMPro;
+using UnityEngine;
+
+public class astroid : MonoBehaviour
+{
+
+    private int id;
+    [SerializeField] private TextMeshProUGUI wordText;
+
+    [SerializeField] float minSpeed, maxSpeed;
+
+    private float speed;
+
+    [SerializeField] private AstroidManager manager;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        speed = Random.Range(minSpeed, maxSpeed);
+
+        manager = FindAnyObjectByType<AstroidManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Time.deltaTime * speed, 0, 0);
+    }
+
+    public void SetType(int type, string word)
+    {
+        id = type;
+        if(type != 3)
+        {
+            wordText.text = word;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "bullet")
+        {
+            if(id == 0)
+            {
+                manager.WonGame();
+            }else if(id == 1)
+            {
+                manager.LostGame();
+            }
+
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+            
+        }
+    }
+
+}
---
+++ Assets/Minigames/Astroids/player.cs
@@ -0,0 +1,102 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+
+    private float playerDirection;
+
+    [SerializeField] float xBoundLower, xBoundUpper;
+
+    [SerializeField] float speed;
+
+    [SerializeField] GameObject bulletPrefab;
+
+    [SerializeField] Transform bulletSpawnSpot;
+
+    
+    private bool walkLeft = false;
+    
+    private bool walkRight = false;
+   
+
+    private void OnEnable()
+    {
+        if (PlayerManager.Instance != null)
+        {
+            PlayerManager.Instance.OnActionPressed += HandleAction;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (PlayerManager.Instance != null)
+        {
+            PlayerManager.Instance.OnActionPressed -= HandleAction;
+        }
+    }
+
+    private void HandleAction(string actionName)
+    {
+        
+        if (actionName == "WalkLeftD")
+        {
+            walkLeft = true;
+        }
+       
+        else if (actionName == "WalkRightD")
+        {
+            walkRight = true;
+        }
+       
+        else if (actionName == "WalkLeftC")
+        {
+            walkLeft = false;
+        }
+        
+        else if (actionName == "WalkRightC")
+        {
+            walkRight = false;
+        }
+
+        if(actionName == "LeftClick")
+        {
+            FireBullet();
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerDirection = 0;
+        if (walkLeft)
+        {
+            playerDirection--;
+        }
+        if (walkRight)
+        {
+            playerDirection++;
+        }
+        
+
+
+        if ((transform.position.x <= xBoundLower && playerDirection < 0) || (transform.position.x >= xBoundUpper && playerDirection > 0))
+        {
+            playerDirection = 0;
+        }
+
+        transform.position += new Vector3(playerDirection * speed * Time.deltaTime, 0, 0);
+    }
+
+    private void FireBullet()
+    {
+        Instantiate(bulletPrefab, bulletSpawnSpot.position, Quaternion.identity);
+    }
+}
