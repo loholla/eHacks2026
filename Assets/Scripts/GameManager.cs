74c9c2e8bb510a7729777221a6de20e27eea27bb
root <root@DESKTOP-EDTC5IG.localdomain>
2026-03-07 00:53:46 -0600

    hotfix/ added a speed multiplier variable

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,7 +8,8 @@ public class GameManager : MonoBehaviour
 
     [Header("Mini Game Information")]
     public List<string> minigameScenes;
-    [SerializeField] private string minigameSceneName;
+    [SerializeField] private string currentMinigame;
+    public float speedMultipler;
 
     [Header("Player Stats")]
     public int playerHealth = 3;
@@ -39,5 +40,25 @@ public class GameManager : MonoBehaviour
         {
             Debug.Log("Game Over");
         }
+    }
+
+    public async void StartNextMiniGame()
+    {
+        if (!string.IsNullOrEmpty(currentMinigame))
+        {
+            await SceneManager.UnloadSceneAsync(currentMinigame);
+        }
+
+        int index = Random.Range(0, minigameScenes.Count);
+        currentMinigame = minigameScenes[index];
+
+        await SceneManager.LoadSceneAsync(currentMinigame, LoadSceneMode.Additive);
+    }
+
+    public async void EndMinigame()
+    {
+        await SceneManager.UnloadSceneAsync(currentMinigame);
+
+        SceneManager.LoadScene("TransitionScene");
     }
 }
